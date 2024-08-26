dea27516fa697c71e22436dcfddd87b2a96e9694
nikhalrajanp62 <nikhalrajanp@gmail.com>
2024-08-26 10:27:49 +0530

    user management initial commit

---
+++ src/pages/Apps.jsx
@@ -0,0 +1,140 @@
+import { Helmet } from 'react-helmet';
+import { useEffect, useMemo, useState } from 'react';
+import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
+
+function Apps() {
+  const title = 'Apps';
+
+  //data and fetching state
+  const [data, setData] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isRefetching, setIsRefetching] = useState(false);
+  const [rowCount, setRowCount] = useState(0);
+
+  //table state
+  const [columnFilters, setColumnFilters] = useState([]);
+  const [globalFilter, setGlobalFilter] = useState('');
+  const [sorting, setSorting] = useState([]);
+  const [pagination, setPagination] = useState({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (!data.length) {
+        setIsLoading(true);
+      } else {
+        setIsRefetching(true);
+      }
+
+      const url = new URL(
+        '/users',
+        'https://jsonplaceholder.typicode.com/',
+      );
+      url.searchParams.set(
+        'start',
+        `${pagination.pageIndex * pagination.pageSize}`,
+      );
+      url.searchParams.set('size', `${pagination.pageSize}`);
+      url.searchParams.set('filters', JSON.stringify(columnFilters ?? []));
+      url.searchParams.set('globalFilter', globalFilter ?? '');
+      url.searchParams.set('sorting', JSON.stringify(sorting ?? []));
+
+      try {
+        const response = await fetch(url.href);
+        const json = await response.json();
+        setData(json);
+        setRowCount(10);
+      } catch (error) {
+        setIsError(true);
+        console.error(error);
+        return;
+      }
+      setIsError(false);
+      setIsLoading(false);
+      setIsRefetching(false);
+    };
+    fetchData();
+  }, [
+    columnFilters,
+    globalFilter,
+    pagination.pageIndex,
+    pagination.pageSize,
+    sorting
+  ]);
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'id',
+        header: 'Id'
+      },
+      {
+        accessorKey: 'name',
+        header: 'Name'
+      },
+      {
+        accessorKey: 'username',
+        header: 'User Name'
+      },
+      {
+        accessorKey: 'email',
+        header: 'Email'
+      },
+      {
+        accessorKey: 'phone',
+        header: 'Phone'
+      },
+    ], []
+  );
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    enableRowSelection: true,
+    getRowId: (row) => row.id,
+    initialState: { showColumnFilters: true },
+    manualFiltering: true,
+    manualPagination: true,
+    manualSorting: true,
+    muiToolbarAlertBannerProps: isError
+      ? {
+          color: 'error',
+          children: 'Error loading data',
+        }
+      : undefined,
+    onColumnFiltersChange: setColumnFilters,
+    onGlobalFilterChange: setGlobalFilter,
+    onPaginationChange: setPagination,
+    onSortingChange: setSorting,
+    rowCount,
+    state: {
+      columnFilters,
+      globalFilter,
+      isLoading,
+      pagination,
+      showAlertBanner: isError,
+      showProgressBars: isRefetching,
+      sorting,
+    },
+  });
+
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <div className="container-fluid">
+        <div
+          className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 className="h2">{title}</h1>
+        </div>
+      </div>
+      <MaterialReactTable table={table} />
+    </>
+  );
+};
+
+export default Apps;
